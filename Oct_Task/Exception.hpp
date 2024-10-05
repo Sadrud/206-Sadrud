9ca9d1318d52ed99d38d9c347850ebb6e94612d6
Ахмедов Садрудин Ахмедович <Akhmedov_SA@ns105.class2.math.msu.su>
2024-10-05 13:58:55 +0300

    задачи про наследование и полиморизм. Решение задачи про нахождение НОК и НОД

---
+++ Oct_Task/Exception.hpp
@@ -0,0 +1,21 @@
+#ifndef EXCEPTION_HPP
+#define EXCEPTION_HPP
+
+#include <iostream>
+#include <ostream>
+#include <string>
+
+class Exception {
+	private:
+		int error_;
+		std::string Message_;
+		Exception () {}
+
+	public:
+		Exception (int error, std::string message) :  error_(error), Message_(message) {}
+		~Exception () {}
+		void print_message () { std::cout <<  "Ошибка " << error_ << ". " << Message_ << std::endl; }
+};
+
+#endif
+
