24925c487fa1d81cfa0481e835723f6a84bc5d3a
Sadrud <ahmedov.sadrudin1@gmail.com>
2025-03-28 07:56:42 +0300

    добавлено сравнение double

--- Mar_Task/include/Geometry/Point.hpp
+++ Mar_Task/include/Geometry/Point.hpp
@@ -4,13 +4,20 @@
 #include <iostream>
 #include <fstream>
 
+static double eps = 1e-15;
+static double abs (double x, double y) {
+	if (x - y >= 0)
+		return x-y;
+	return y-x;
+}
+
 class Point {
 	public:
 		double x;
 		double y;
 		Point (double x_ = 0, double y_ = 0) : x(x_), y(y_) {}
 		~Point () {}
-		bool operator== (Point& other) { return ((x == other.x) || (y == other.y)); }
+		bool operator== (Point& other) { return (abs(x, other.x) <= eps || abs(y, other.y) <= eps); }
 		bool operator< (Point& other) { return  ((x < other.x) || (y < other.y)); }
 };
 
