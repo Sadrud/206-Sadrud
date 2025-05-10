ae8edd6465e184d0249ec3325562fdb4370e427a
Sadrud <ahmedov.sadrudin1@gmail.com>
2025-05-10 20:22:39 +0300

    тесты для тестирования

---
+++ Apr_Task/tests/tests.cpp
@@ -0,0 +1,28 @@
+/*!
+	\file tests.cpp
+	\brief Создание тестов.
+	\author Sadrud
+	\date 2025
+*/
+
+
+#include "../methods/methods.cpp"
+
+/*!
+	\brief Пространство имен для тестов
+	\details В тесте создается матрица смежности. Также создается название файла, в котором будет хранится матрица и эйлеров путь.
+	\param [in] folder, vertices, maxEdges, density Название папки, в которой будут хранится матрицы смежности, количесвто вершин, максимальное количество ребер, выходящих из одной вершины, и плотность распределения ребер по вершинам
+*/
+
+namespace GraphTests {
+	void randomTest (std::string folder, int vertices, int maxEdges, float density) {
+		std::vector<std::vector<int>> g = generateAdjacencyMatrix(vertices, density, maxEdges);
+
+		std::string filename = 
+			"The adjacency matrix of the graph: vertices - " + std::to_string(vertices) 
+			+ ", edge density - " + std::to_string(density) 
+			+ ", max count of edges - " + std::to_string(maxEdges);
+
+		printMatrix (folder, filename, g);
+	}
+}
